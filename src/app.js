5e4d951669a188880b87a6fe994c6d295d792e26
Shivam Gupta <shivamgupta2300@gmail.com>
2024-11-03 23:10:27 +0530

    added get, post and delete user Api

--- src/app.js
+++ src/app.js
@@ -9,12 +9,52 @@ app.use(express.json())
 
 app.post('/signup', async (req, res) => {
     const user = new User(req.body)
-
     try {
         await user.save()
         res.send('data is saved')
     }
     catch (err){
+        res.status(400).send('error connecting to database' + err?.message)
+    }
+})
+
+app.get('/user', async(req, res) => {
+    const email = req.body.emailId
+    try {
+        const data = await User.find({emailId: email})
+        res.send('data is found by email')
+    } catch (err){
+        res.status(400).send('error connecting to database' + err?.message)
+    }
+})
+
+app.get('/feed', async(req, res) => {
+    try {
+        const data = await User.find({})
+        res.send('all feed data is shown')
+    } catch (err){
+        res.status(400).send('error connecting to database' + err?.message)
+    }
+})
+
+app.delete('/user' , async(req, res) => {
+    const userId = req.body.userId
+    try {
+        const data = await User.findByIdAndDelete(userId)
+        res.send('data after deletion')
+    } catch {
+        res.status(400).send('error connecting to database' + err?.message)
+    }
+})
+
+app.patch('/user' , async(req, res) => {
+    const userId = req.body.userId
+    const Data = req.body
+    try {
+        const data = await User.findByIdAndUpdate(userId, Data)
+        console.log('@@@', data)
+        res.send('data after updation')
+    } catch {
         res.status(400).send('error connecting to database' + err?.message)
     }
 })
